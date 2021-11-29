43e1ef3918160aea21beee3aa7311be84ec65cd8
srosenberg74 <srosenberg@alphaworks.tech>
2021-11-29 15:53:30 -0800

    feat(list-container): Add additem functionality #7

--- src/App.js
+++ src/App.js
@@ -3,20 +3,50 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const [listContainer, setListContainer] = useState(["Hey", "How are you"]);
+  const [listContainer, setListContainer] = useState([]);
+  const [text, onChangeText] = useState("");
+  const [placeholder, setPlaceholder] = useState("enter item here");
 
   const generateList = listContainer.map((items, index) => (
-    <div>
+    <div key={index}>
       <h3>{items}</h3>
     </div>
   ));
+
+  const addItem = (event) => {
+    if (text.trim().length > 0) {
+      setListContainer([...listContainer, text.trim()]);
+      setPlaceholder("enter item here");
+      onChangeText("");
+      event.preventDefault();
+      console.log("listContainer=", listContainer);
+    } else {
+      setPlaceholder("Invalid entry. Item was blank");
+      event.preventDefault();
+    }
+  };
+
+  const handleChange = (event) => {
+    onChangeText(event.target.value);
+    event.preventDefault();
+    // console.log("changeText=", text);
+  };
 
   return (
     <div className="App">
       <header className="App-header">Decidr</header>
       <div className="list-div">{generateList}</div>
-      <input type="text"></input>
-      <button>Add Item</button>
+      <form onSubmit={addItem}>
+        <input
+          type="text"
+          value={text}
+          onChange={handleChange}
+          placeholder={placeholder}
+          // placeholder="enter item here"
+          autoFocus
+        ></input>
+        <button type="submit">Add Item</button>
+      </form>
     </div>
   );
 }
