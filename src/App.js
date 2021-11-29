c23256830f22e2609174ff86be02ed6ed7a4f4d8
srosenberg74 <srosenberg@alphaworks.tech>
2021-11-29 14:19:20 -0800

    chore(list-container): added list container #4

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [listContainer, setListContainer] = useState(["Hey", "How are you"]);
+
+  const generateList = listContainer.map((items, index) => (
+    <div>
+      <h3>{items}</h3>
+    </div>
+  ));
+
+  return (
+    <div className="App">
+      <header className="App-header">Decidr</header>
+      <div className="list-div">{generateList}</div>
+    </div>
+  );
+}
+
+export default App;
